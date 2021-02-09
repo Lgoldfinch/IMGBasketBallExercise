e6febb903d2f79fe9dbab307755f8aeb640b334f
Louis Goldfinch <louis.goldfinch@digital.hmrc.gov.uk>
2021-02-09 17:28:59 +0000

    Fixed validation method

--- src/main/scala/validation/ValidateData.scala
+++ src/main/scala/validation/ValidateData.scala
@@ -1,6 +1,6 @@
 package validation
 
-import models.{Event, Events}
+import models.{Event, Events, Team1, Team2}
 
 import scala.annotation.tailrec
 
@@ -18,20 +18,28 @@ object ValidateData {
 
 
   @tailrec
-  final def compareEvents(oldEvent: Event, newEvent: Event): Event = {
-    (newEvent, oldEvent) match {
+  final def compareEvents(prevEvent: Event, newEvent: Event): Event = {
+    (newEvent, prevEvent) match {
       case (newEvent, oldEvent) if newEvent.team1Total < oldEvent.team1Total =>
-        compareEvents(newEvent.copy(team1Total = oldEvent.team1Total),  oldEvent )
+        compareEvents(oldEvent, newEvent.copy(team1Total = oldEvent.team1Total) )
 
       case (newEvent, oldEvent) if newEvent.team2Total < oldEvent.team2Total =>
-        compareEvents(newEvent.copy(team2Total = oldEvent.team2Total), oldEvent)
+        compareEvents(oldEvent, newEvent.copy(team2Total = oldEvent.team2Total))
 
-      case (newEvent, oldEvent) if newEvent.elapsedMatchTime < oldEvent.elapsedMatchTime =>
-        compareEvents(newEvent.copy(elapsedMatchTime = oldEvent.elapsedMatchTime), oldEvent)
+      case (newEvent, prevEvent) if newEvent.elapsedMatchTime < prevEvent.elapsedMatchTime =>
+        compareEvents(prevEvent, newEvent.copy(elapsedMatchTime = prevEvent.elapsedMatchTime))
 
       case _ => newEvent
     }
   }
 
+  private def handleInconsistentTeamTotals(oldEvent: Event, newEvent: Event): Int = {
+    val previousTeam1Total = oldEvent.team1Total
+    val previousTeam2Total = oldEvent.team2Total
 
+      newEvent.whoScored match {
+      case Team1 => previousTeam1Total + newEvent.pointsScored
+      case Team2 => previousTeam2Total + newEvent.pointsScored
+    }
+  }
 }
--- src/test/scala/validation/ValidateDataSpec.scala
+++ src/test/scala/validation/ValidateDataSpec.scala
@@ -6,8 +6,20 @@ import ValidateData._
 
 class ValidateDataSpec extends TestHelper {
 
-  "bob" in {
-//    val inconsistentList: Events = Events(twoEvents.values.reverse)
-//    validateEvents(inconsistentList) shouldBe ""
+  "validateEvents" should {
+    "correct a list of events with inconsistent data" in {
+      val inconsistentList: Events = Events(twoEvents.values.reverse)
+      val team1total = event2.team1Total
+      val team2total = event2.team2Total
+      val elapsedTime = event2.elapsedMatchTime
+      val validatedEvent: Event = event1.copy(
+        team2Total = team2total,
+        team1Total = team1total,
+        elapsedMatchTime = elapsedTime)
+
+      val validatedEvents = Events(List(event2, validatedEvent))
+
+      validateEvents(inconsistentList) shouldBe validatedEvents
+    }
   }
 }
